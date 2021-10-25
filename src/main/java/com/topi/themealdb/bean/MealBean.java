0da70c719fc501254e3bd930cd571148c6083df2
EnzoSoares73 <enzosoutosoares@gmail.com>
2021-10-25 01:06:04 -0300

    Visualizar funcional

--- src/main/java/com/topi/themealdb/bean/MealBean.java
+++ src/main/java/com/topi/themealdb/bean/MealBean.java
@@ -4,25 +4,32 @@ import com.topi.themealdb.modelo.Meal;
 import com.topi.themealdb.servico.MealServico;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 @Named
-@ViewScoped
+@SessionScoped
 public class MealBean implements Serializable {
 
     private MealServico mealServico = new MealServico();
 
     private List<Meal> listaMeals;
+    private Meal meal;
 
     @PostConstruct
     public void inicializaListagem() throws IOException {
         listaMeals = mealServico.retornaTodasMeals();
+    }
+
+    public void visualizarMeal(int id) throws IOException {
+        setMeal(mealServico.obterMealPorId(id));
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        context.redirect(context.getRequestContextPath() + "/webpages/meal.xhtml");
     }
 
     public List<Meal> getListaMeals() {
@@ -31,5 +38,13 @@ public class MealBean implements Serializable {
 
     public void setListaMeals(List<Meal> listaMeals) {
         this.listaMeals = listaMeals;
+    }
+
+    public Meal getMeal() {
+        return meal;
+    }
+
+    public void setMeal(Meal meal) {
+        this.meal = meal;
     }
 }
--- src/main/java/com/topi/themealdb/servico/MealServico.java
+++ src/main/java/com/topi/themealdb/servico/MealServico.java
@@ -1,5 +1,6 @@
 package com.topi.themealdb.servico;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.topi.themealdb.modelo.Meal;
@@ -9,26 +10,25 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MealServico {
 
-    public final String ENDERECO_API = "https://www.themealdb.com/api/json/v1/";
-    public final String CHAVE_API = "1";
-    public final String PROCURAR_POR_NOME = "/search.php?s=";
-    public final int NUMERO_INGREDIENTES = 20;
-    public final String VAR_INGREDIENTE = "strIngredient";
-    public final String VAR_MEDIDA = "strMeasure";
+    private final String ENDERECO_API = "https://www.themealdb.com/api/json/v1/";
+    private final String CHAVE_API = "1";
+    private final String PROCURAR_POR_NOME = "/search.php?s=";
+    private final String PROCURAR_POR_ID = "/lookup.php?i=";
+    private final int NUMERO_INGREDIENTES = 20;
+    private final String VAR_INGREDIENTE = "strIngredient";
+    private final String VAR_MEDIDA = "strMeasure";
 
-    public List<Meal> retornaTodasMeals() throws IOException {
-        URL url = new URL(ENDERECO_API + CHAVE_API + PROCURAR_POR_NOME);
+    private HttpURLConnection conexao;
 
+    private StringBuilder conectar(URL url) throws IOException {
         HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
         conexao.setRequestMethod("GET");
         conexao.connect();
@@ -38,50 +38,61 @@ public class MealServico {
         if (responsecode != 200) {
             throw new RuntimeException("HttpResponseCode: " + responsecode);
         } else {
-
             StringBuilder inline = new StringBuilder();
             Scanner scanner = new Scanner(url.openStream());
 
-            //Write all the JSON data into a string using a scanner
             while (scanner.hasNext()) {
                 inline.append(scanner.nextLine());
             }
 
-            //Close the scanner
             scanner.close();
+            return inline;
+        }
+    }
 
-            try {
-                JSONObject obj = (JSONObject) new JSONParser().parse(String.valueOf(inline));
+    public List<Meal> retornaTodasMeals() throws IOException {
+        URL url = new URL(ENDERECO_API + CHAVE_API + PROCURAR_POR_NOME);
+        return jsonParaListaMeals(conectar(url));
+    }
 
-                ObjectMapper mapper = new ObjectMapper();
-                List<Meal> meals = mapper.readValue(obj.get("meals").toString(), new TypeReference<List<Meal>>(){});
-                JSONArray jsonArray = (JSONArray) obj.get("meals");
+    public Meal obterMealPorId(int id) throws IOException {
+        URL url = new URL(ENDERECO_API + CHAVE_API + PROCURAR_POR_ID + id);
+        return Objects.requireNonNull(jsonParaListaMeals(conectar(url))).get(0);
+    }
 
-                for (int i = 1; i < meals.size(); i++) {
-                    //Converte o atributo strTags do json para uma lista tags dentro do objeto
-                    JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                    if (jsonObject.get("strTags") != null) {
-                        String tags = jsonObject.get("strTags").toString();
-                        tags = tags.replace(" ", "");
-                        meals.get(i).setTags(tags.split(","));
-                    }
+    private List<Meal> jsonParaListaMeals(StringBuilder inline) {
+        try {
+            JSONObject obj = (JSONObject) new JSONParser().parse(String.valueOf(inline));
 
-                    //converte os atributos strIngredientx e strMeasurex em um hashmap<strIngredientx, strMeasurex>
-                    for (int j = 1; j <= NUMERO_INGREDIENTES; j++) {
-                        if ((jsonObject.get(VAR_INGREDIENTE + j) != null && jsonObject.get(VAR_MEDIDA + j) != null) &&
-                            (!jsonObject.get(VAR_INGREDIENTE + j).toString().isEmpty() && !jsonObject.get(VAR_MEDIDA + j).toString().isEmpty())) {
-                            meals.get(i).getIngredients_amount().put(jsonObject.get(VAR_INGREDIENTE + j).toString(), jsonObject.get(VAR_MEDIDA + j).toString());
-                        } else {
-                            break;
-                        }
-                    }
+            ObjectMapper mapper = new ObjectMapper();
+            List<Meal> meals = mapper.readValue(obj.get("meals").toString(), new TypeReference<List<Meal>>() {
+            });
+            JSONArray jsonArray = (JSONArray) obj.get("meals");
+
+            for (int i = 1; i < meals.size(); i++) {
+                //Converte o atributo strTags do json para uma lista tags dentro do objeto
+                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+                if (jsonObject.get("strTags") != null) {
+                    String tags = jsonObject.get("strTags").toString();
+                    tags = tags.replace(" ", "");
+                    meals.get(i).setTags(tags.split(","));
                 }
 
-                return meals;
-
-            } catch (ParseException e) {
-                e.printStackTrace();
+                //converte os atributos strIngredientx e strMeasurex em um hashmap<strIngredientx, strMeasurex>
+                for (int j = 1; j <= NUMERO_INGREDIENTES; j++) {
+                    if ((jsonObject.get(VAR_INGREDIENTE + j) != null && jsonObject.get(VAR_MEDIDA + j) != null) &&
+                            (!jsonObject.get(VAR_INGREDIENTE + j).toString().isEmpty() && !jsonObject.get(VAR_MEDIDA + j).toString().isEmpty())) {
+                        meals.get(i).getIngredients_amount().put(jsonObject.get(VAR_INGREDIENTE + j).toString(), jsonObject.get(VAR_MEDIDA + j).toString());
+                    } else {
+                        break;
+                    }
+                }
             }
+
+            return meals;
+
+        } catch (ParseException | JsonProcessingException e) {
+            e.printStackTrace();
         }
         return null;
     }
